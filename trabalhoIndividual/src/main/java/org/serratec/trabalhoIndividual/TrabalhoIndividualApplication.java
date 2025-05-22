b27a6c999f4ea19b8b85dca410cda3688f44ccf7
Nikolas Wintter <nkwintter@gmail.com>
2025-05-22 18:23:04 -0300

    Add files via upload

---
+++ trabalhoIndividual/src/main/java/org/serratec/trabalhoIndividual/TrabalhoIndividualApplication.java
@@ -0,0 +1,13 @@
+package org.serratec.trabalhoIndividual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoIndividualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoIndividualApplication.class, args);
+	}
+
+}
